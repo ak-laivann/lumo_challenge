3a40fda5a8595ebc14441038a12ac96a612e5ef8
Anantha Krishnan G <ananthu0601@gmail.com>
2025-07-06 11:40:29 +0530

    the disability function and the reset fields and on submit function handler has been created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { RootRouter } from "./RootRouter";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <BrowserRouter>
       <RootRouter />
+
+      {/* // toast container is required for the toast to work */}
+      <ToastContainer position="top-center" />
     </BrowserRouter>
   );
 }
--- src/BookFlightsForm.tsx
+++ src/BookFlightsForm.tsx
@@ -1,38 +1,102 @@
-import { Button, DatePicker, Form, Input, Radio } from "antd";
-import { useState } from "react";
+import { Button, DatePicker, Form, Radio } from "antd";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import dayjs from "dayjs";
 
 export const BookFlightsForm = () => {
   const [type, setType] = useState("SINGLE");
   const [form] = Form.useForm();
+  const [isDisabled, setIsDisabled] = useState(true);
+  const departureDate = Form.useWatch("departure", form);
+
+  // check fields to disable the submit button
+  const checkFields = (values: any) => {
+    const { departure, return: returnDate } = values;
+    const isValid =
+      type === "SINGLE" ? !!departure : !!departure && !!returnDate;
+    setIsDisabled(!isValid);
+  };
+
+  // this function would be run on submitting
+  const handleFinish = (values: any) => {
+    const departureDate = values.departure.format("YYYY-MM-DD");
+    if (type === "SINGLE") {
+      toast.success(`You have booked the ticket for ${departureDate}`);
+    } else {
+      const returnDate = values.return.format("YYYY-MM-DD");
+      toast.success(
+        `You have booked the ticket for ${departureDate} and ${returnDate}`
+      );
+    }
+
+    // after the backend call or in this case toast, we need to reset the fields and simulate the default behavior
+    form.resetFields();
+    setType("SINGLE");
+    setIsDisabled(true);
+  };
+
+  // this disables past dates
+  const disablePastDates = (current: any) => {
+    return current && current < dayjs().startOf("day");
+  };
+
+  // this is for return date picker
+  const disableReturnDates = (current: any) => {
+    if (!departureDate) return disablePastDates(current);
+    return (
+      current < dayjs().startOf("day") ||
+      current < dayjs(departureDate).startOf("day")
+    );
+  };
+
   return (
     <div style={{ padding: "50px", border: "10px solid black" }}>
       <Form
         layout="vertical"
-        onFinish={(values) => {
-          console.log(values);
-        }}
         form={form}
+        onFinish={handleFinish}
+        onValuesChange={(_, allValues) => checkFields(allValues)}
       >
         <Form.Item name={"type"} label={"Type of Flight"}>
           <Radio.Group
+            defaultValue={type}
             options={[
               { label: "One-Way Flight", value: "SINGLE" },
               { label: "Return Flight", value: "RETURN" },
             ]}
             value={type}
-            onChange={(e) => setType(e.target.value)}
-          ></Radio.Group>
+            onChange={(e) => {
+              setType(e.target.value);
+              if (e.target.value === "SINGLE") {
+                form.setFieldsValue({ return: undefined });
+              }
+            }}
+          />
         </Form.Item>
+
         <h1>You are booking for {type} flight</h1>
-        <Form.Item name={"departure"} label={"Departure"}>
-          <DatePicker />
+
+        <Form.Item
+          name={"departure"}
+          label={"Departure"}
+          rules={[
+            { required: true, message: "Please select a departure date" },
+          ]}
+        >
+          <DatePicker disabledDate={disablePastDates} />
         </Form.Item>
+
         {type === "RETURN" && (
-          <Form.Item name={"return"} label={"Return"}>
-            <DatePicker />
+          <Form.Item
+            name={"return"}
+            label={"Return"}
+            rules={[{ required: true, message: "Please select a return date" }]}
+          >
+            <DatePicker disabledDate={disableReturnDates} />
           </Form.Item>
         )}
-        <Button type="primary" htmlType="submit" disabled={false}>
+
+        <Button type="primary" htmlType="submit" disabled={isDisabled}>
           Book
         </Button>
       </Form>
